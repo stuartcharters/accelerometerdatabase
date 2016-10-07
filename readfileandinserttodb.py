e996bc6b29e21b53d1f08e7f99793f52da5a8185
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-10-07 14:38:27 +1000

    change to 2:3

--- readfileandinserttodb.py
+++ readfileandinserttodb.py
@@ -27,7 +27,7 @@ with con:
         with open(file,'rb') as csvfile:
             for row in csv.reader(csvfile):
                     str = str(row[:1])
-                    print str[3:3]
+                    print str[2:3]
                     sys.exit(0)
 #                    if row[:1]==';':
 #                        print row
