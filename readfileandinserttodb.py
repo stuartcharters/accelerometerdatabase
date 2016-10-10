2ea408150c908024f65a0981b0a2b9be1b6e7128
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-10-10 11:28:44 +1000

    split date string into own variable for ease of debugging

--- readfileandinserttodb.py
+++ readfileandinserttodb.py
@@ -76,7 +76,8 @@ with con:
                         timeincrement = datetime.timedelta(seconds=t)
                         newtime = basetime + timeincrement
                         #print(basetime,t,newtime)
-                        sqlstring = 'INSERT into movementdata VALUES(' + snum +',' +newtime.strftime(%Y-%m-%d %H:%M:%S:%f)  +',' + temperature +',' + x +',' + y +',' + z + ')'
+                        datestring = newtime.strftime(%Y-%m-%d %H:%M:%S:%f)
+                        sqlstring = 'INSERT into movementdata VALUES(' + snum +',' + datestring +',' + temperature +',' + x +',' + y +',' + z + ')'
 
 
 ## db stuff - temp for reference
