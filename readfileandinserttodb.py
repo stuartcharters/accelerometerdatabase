a0635c291d63be78c230db4db1a682668580a886
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-10-10 11:17:18 +1000

    formatted sql string

--- readfileandinserttodb.py
+++ readfileandinserttodb.py
@@ -75,7 +75,8 @@ with con:
                         z = row[3]
                         timeincrement = datetime.timedelta(seconds=t)
                         newtime = basetime + timeincrement
-                        print(basetime,t,newtime)
+                        #print(basetime,t,newtime)
+                        sqlstring = 'INSERT into movementdata VALUES(' + snum +',' + \' +newtime.strftime(%Y-%m-%d %H:%M:%S:%f) + \' +',' + temperature +',' + x +',' + y +',' + z + ')'
 
 
 ## db stuff - temp for reference
